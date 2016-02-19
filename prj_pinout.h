e5584f46222b55f5e7e3073b6af977df5091ff9f
Txinto Vaz <txinto@gmail.com>
2016-02-19 21:02:44 +0100

    Re-pin assignement to take profit of the bottom side pins

--- prj_pinout.h
+++ prj_pinout.h
@@ -7,35 +7,35 @@
 #include "gttc_types.h"
 
 /**** Pin assigment section ****/
-#define CFG_POWERGND_PIN 2
+#define CFG_POWERGND_PIN 2 // anteriormente 2
 
-#define CFG_JOY_UP_PIN 23
-#define CFG_JOY_DOWN_PIN 21
-#define CFG_JOY_LEFT_PIN 22
-#define CFG_JOY_RIGHT_PIN 20
+#define CFG_JOY_UP_PIN 23 // anteriormente 23
+#define CFG_JOY_DOWN_PIN 21 // anteriormente 21
+#define CFG_JOY_LEFT_PIN 22 // anteriormente 22
+#define CFG_JOY_RIGHT_PIN 20 // anteriormente 20
 
-#define CFG_JOY_GREENBUT_PIN 19
-#define CFG_JOY_REDBUT_PIN 18
-#define CFG_JOY_BLUEBUT_PIN 17
-#define CFG_JOY_YELLOWBUT_PIN 16
-#define CFG_JOY_GREYBUT_PIN 15
-#define CFG_JOY_BLACKBUT_PIN 14
-#define CFG_JOY_WHITELBUT_PIN 12
-#define CFG_JOY_WHITERBUT_PIN 11
-#define CFG_JOY_MODEBUT_PIN 10
-#define CFG_JOY_RESTARTBUT_PIN 9
-#define CFG_JOY_PLAYERBUT_PIN 8
+#define CFG_JOY_GREENBUT_PIN 19// anteriormente 19
+#define CFG_JOY_REDBUT_PIN 18 // anteriormente 18
+#define CFG_JOY_BLUEBUT_PIN 17 // anteriormente 17
+#define CFG_JOY_YELLOWBUT_PIN 16 // anteriormente 16
+#define CFG_JOY_GREYBUT_PIN 15 // anteriormente 15
+#define CFG_JOY_BLACKBUT_PIN 14 // anteriormente 14
+#define CFG_JOY_WHITELBUT_PIN 29 // anteriormente 12
+#define CFG_JOY_WHITERBUT_PIN 30 // anteriormente 11
+#define CFG_JOY_MODEBUT_PIN 31 // anteriormente 10
+#define CFG_JOY_RESTARTBUT_PIN 32 // anteriormente 9
+#define CFG_JOY_PLAYERBUT_PIN 33 // anteriormente 8
 
 /**** ATARI norm output ****/
-#define CFG_ATARI_PIN_1 7 // MSX FW
-#define CFG_ATARI_PIN_2 6 // MSX BACK
-#define CFG_ATARI_PIN_3 5 // MSX LEFT
-#define CFG_ATARI_PIN_4 4 // MSX RIGHT
-//#define CFG_ATARI_PIN_5   // MSX +5v
-#define CFG_ATARI_PIN_6 3  // MSX TRG1
-#define CFG_ATARI_PIN_7 1  // MSX TRG2
-#define CFG_ATARI_PIN_8 0   // MSX OUT
-//#define CFG_ATARI_PIN_9   // MSX GND
+#define CFG_ATARI_PIN_1 9 // anteriormente 7  // MSX FW // AZUL 1er conector
+#define CFG_ATARI_PIN_2 8 // anteriormente 6  // MSX BACK // VERDE 1er conector
+#define CFG_ATARI_PIN_3 7 // anteriormente 5  // MSX LEFT // AMARILLO 1er conector
+#define CFG_ATARI_PIN_4 6 // anteriormente 4  // MSX RIGHT // BLANCO 1er conector
+#define CFG_ATARI_PIN_5 5 // anteriormente NC // MSX +5v // ROJO 1er conector
+#define CFG_ATARI_PIN_6 12 // anteriormente 3  // MSX TRG1 // AZUL 2o conector
+#define CFG_ATARI_PIN_7 11 // anteriormente 1  // MSX TRG2 // VERDE 2o conector
+#define CFG_ATARI_PIN_8 10 // anteriormente 0  // MSX OUT // AMARILLO 2o conector
+#define CFG_ATARI_PIN_9 4 // anteriormente NC // MSX GND // NEGRO 1er conector
 
 /* Prepares the pinout for project use */
 void pinoutInit(void);
